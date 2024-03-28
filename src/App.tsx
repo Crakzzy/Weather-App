13550689013953cb3e6cf73cec9d0cb9b8fda42e
Valko Martin <matinko.valko@gmail.com>
2024-03-28 18:54:42 +0100

    Working current temperature fetching

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {useEffect, useState} from "react";
+import getCurrentTemperature from "./fetchApi";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const [currentTemperature, setCurrentTemperature] = useState(null);
+
+    useEffect(() => {
+        getCurrentTemperature().then(temperature => {
+            setCurrentTemperature(temperature);
+        });
+    }, []);
+
+
+    return (
+        <div>
+            <p>{currentTemperature}</p>
+        </div>
+    );
 }
 
 export default App;
